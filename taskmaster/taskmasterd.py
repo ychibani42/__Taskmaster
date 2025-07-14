44be7b0ca2cadc29014a9f0d158729d4b949d911
Yassine CHIBANI <ychibani@f2r2s15.clusters.42paris.fr>
2025-07-14 17:47:23 +0200

    Parsing unfinished yet

--- taskmaster/taskmasterd.py
+++ taskmaster/taskmasterd.py
@@ -1,40 +1,24 @@
-from pydantic import BaseModel, ValidationError
-from typing import Dict, List, Union, Optional
+from models import ProgramConfig
+from pydantic import ValidationError
 import configparser
+import subprocess
+from typing import Dict, List
+import time
 
 
-class ProgramConfig(BaseModel):
-    cmd: str
-    numprocs: int = 1
-    umask: Union[int, str] = "022"
-    workingdir: str
-    autostart: bool = True
-    autorestart: str = "unexpected"
-    exitcodes: Union[int, List[int]] = 0
-    startretries: int = 3
-    starttime: int = 5
-    stopsignal: str = "TERM"
-    stoptime: int = 10
-    stdout: Optional[str] = None
-    stderr: Optional[str] = None
-    env: Optional[Dict[str, str]] = None
-
-
-def parse_exitcodes(value):
+def parse_exitcodes(value: str) -> List[int]:
     if ',' in value:
         return [int(x.strip()) for x in value.split(',')]
-    try:
-        return int(value)
-    except ValueError:
-        return [int(x) for x in value.split()]
+    else:
+        return int(value.strip())
 
 
-def parse_env(value):
+def parse_env(value: str) -> Dict[str, str]:
     env_dict = {}
     for pair in value.split(','):
         if '=' in pair:
-            k, v = pair.split('=', 1)
-            env_dict[k.strip()] = v.strip()
+            key, k_value = pair.split("=")
+            env_dict[key.strip()] = k_value.strip()
     return env_dict if env_dict else None
 
 
@@ -43,24 +27,31 @@ def load_program_configs(path: str) -> Dict[str, ProgramConfig]:
     config.read(path)
     programs = {}
     for section in config.sections():
-        data = dict(config.items(section))
-
-        if 'exitcodes' in data:
-            data['exitcodes'] = parse_exitcodes(data['exitcodes'])
-        if 'env' in data:
-            data['env'] = parse_env(data['env'])
-
+        Data = dict(config.items(section))
+        if 'exitcodes' in Data:
+            Data["exitcodes"] = parse_exitcodes(Data["exitcodes"])
+        if 'env' in Data:
+            Data['env'] = parse_env(Data['env'])
         try:
-            programs[section] = ProgramConfig(**data)
-        except ValidationError as e:
-            print(f"Error in section [{section}]:\n{e}\n")
+            programs[section] = ProgramConfig(**Data)
+        except ValidationError as exc:
+            print(repr(exc.errors()[0]['type']))
     return programs
+
+
+def launch_programs(Config: ProgramConfig):
+    process = subprocess.Popen(Config["cmd"])
+    time.sleep(2)
+    if process.poll() is None:
+        print("Le processus est toujours vivant")
+    else:
+        print("Le processus est terminé")
+    return 1
 
 
 def main():
     configs = load_program_configs('foo.conf')
-    for name, cfg in configs.items():
-        print(f"{name}: {cfg}")
+    launch_programs(configs)
 
 
 main()
